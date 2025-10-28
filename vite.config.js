005ab9db8a5253e879f9d327784f84c238822245
Avnish kumar <avnishkumar4605@gmail.com>
2025-10-29 00:37:58 +0530

    some minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  server:{
-    port:3000
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    proxy: {
+      // 👇 When your frontend makes a request to /api/...,
+      // it will be proxied to your backend.
+      "/api": {
+        target: "http://localhost:8080", // your backend server URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""), // remove /api prefix if needed
+      },
+    },
   },
-})
+});
