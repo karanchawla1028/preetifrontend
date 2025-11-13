e358294212bee071cbb53e5580c83ddcd1d1a279
Avnish kumar <avnishkumar4605@gmail.com>
2025-11-13 22:23:20 +0530

    some minor change

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-
-    // 👇 Allow specific production domains
     allowedHosts: ["www.preetinest.ca", "preetinest.ca"],
-
     proxy: {
       "/api": {
-        // 👇 Your backend target for local dev
         target: "http://localhost:8080",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
