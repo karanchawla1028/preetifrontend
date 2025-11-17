f4e4ba843d00d70186fd26087c9495c69511e77f
Avnish kumar <avnishkumar4605@gmail.com>
2025-11-18 00:14:07 +0530

    some minor changes

--- src/httpcommon.js
+++ src/httpcommon.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-
 // export const api = axios.create({
-//   baseURL: "/", // ✅ change to your backend URL
+//   baseURL: "/api",
 //   headers: {
 //     "Content-Type": "application/json",
 //   },
 // });
+
+
+export const api = axios.create({
+  baseURL: "/", // ✅ change to your backend URL
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
