64aa1521fb56fcbf9675b8967c5dc32c8779a28d
Avnish kumar <avnishkumar4605@gmail.com>
2025-11-18 00:03:16 +0530

    some minor changes

--- src/httpcommon.js
+++ src/httpcommon.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+// export const api = axios.create({
+//   baseURL: "/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "/", // ✅ change to your backend URL
   headers: {
     "Content-Type": "application/json",
   },
--- src/toolkit/slices/authSlice.js
+++ src/toolkit/slices/authSlice.js
@@ -1,9 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { api } from "../../httpcommon";
 
-const authSlice=createSlice({
-    name:'auth',
-    initialState:{}
-})
+export const userLogin = createAsyncThunk("userLogin", async () => {
+  const response = await api.post(`/api/login`);
+  return response.data;
+});
 
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    loading: "",
+    userDetail: {},
+  },
+  extraReducers: (builder) => {
+    builder.addCase(userLogin.pending, (state) => {
+      state.loading = "pending";
+    });
+    builder.addCase(userLogin.fulfilled, (state, action) => {
+      state.userDetail = action.payload;
+      state.loading = "success";
+    });
+    builder.addCase(userLogin.rejected, (state) => {
+      state.loading = "reject";
+    });
+  },
+});
 
-export default authSlice.reducer
+export default authSlice.reducer;
