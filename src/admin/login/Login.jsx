79378574c48d901a459513352f308732fbaf5539
Avnish kumar <avnishkumar4605@gmail.com>
2025-11-25 22:48:26 +0530

    some minor changes

--- src/admin/login/Login.jsx
+++ src/admin/login/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     dispatch(userLogin(detail)).then((resp) => {
       if (resp.meta.requestStatus === "fulfilled") {
         alert("User logged in successfully !.");
-        navigate(`/${resp.payload?.id}/admin/dashboard`);
+        // navigate(`/${resp.payload?.id}/admin/dashboard`);
       }else{
         alert("something went wrong !.")
       }
